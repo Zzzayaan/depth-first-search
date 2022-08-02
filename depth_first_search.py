1d0d5b03e2c5af261bf7d544bd96ba3a8e85167b
Zzzayaan <zayaan737@gmail.com>
2022-08-02 20:37:22 +0100

    Create depth_first_search.py
    algorithm implemented!

---
+++ depth_first_search.py
@@ -0,0 +1,33 @@
+# Create an adjacency list
+graph = {
+	'A' : ['B', 'C'],
+	'B' : ['D'],
+	'C'	: ['E', 'F'],
+	'D' : [],
+	'E' : ['G'],
+	'F' : ['H'],
+	'G' : ['H'],
+	'H' : []
+}
+
+# Set contains visited vertices
+visited = set()
+
+# Passes values of the graph, visited vertices, and the starting vertex
+def dfs(visited, graph, vertex):
+	if vertex not in visited:
+		print(vertex)
+		visited.add(vertex) # Appends vertex into visited set
+		for n in graph[vertex]: # dfs is called again for vertex neighbours
+			dfs(visited, graph, n)
+
+test = dfs(visited, graph, 'C')
+print(test)
+
+# Output:
+# C
+# E
+# G
+# H
+# F
+# None
